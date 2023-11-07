38c83b3aafd22655f26c19f619132f23e17f94e7
yanansun <116763187+yanansun0074@users.noreply.github.com>
2023-11-07 10:50:22 -0500

    Update scrolltotop in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import { HashRouter as Router, Routes, Route } from "react-router-dom";
-
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Portfolio from "./Pages/Portfolio";
 import About from "./Pages/About";
